8f54a78817ca72169fc04909ce1c3ee5e2d546e8
Fardan11 <fardankabir11@gmail.com>
2024-02-08 16:18:06 +0600

    '1st'

--- part2.js
+++ part2.js
@@ -1,1 +1,26 @@
 console.log("Just to check ");
+
+//arror function
+// const calAverage = (a, b, c) => (a + b + c) / 3;
+
+//     console.log(calAverage(3,4,5))
+
+const calAverage = function (a, b, c) {
+  return (a + b + c) / 3;
+};
+
+const checkWinner = function (avgDolphines, avgKoalas) {
+  if (avgDolphines >= 2 * avgKoalas) {
+    console.log(`Dolphines win ${avgDolphines} vs ${avgKoalas}`);
+  } else if (avgKoalas >= 2 * avgDolphines) {
+    console.log(`Koalas win `);
+  } else {
+    console.log(`No team wins `);
+  }
+};
+
+const scoreDolphines = calAverage(44, 23, 71);
+const scoreKoalas = calAverage(65, 54, 49);
+
+console.log(checkWinner(scoreDolphines, scoreKoalas));
+console.log(checkWinner(544, 111));
